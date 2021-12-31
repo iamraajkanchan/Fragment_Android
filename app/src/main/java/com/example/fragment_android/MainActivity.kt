16d2b2c0a100d616a8a85eb7c2aa5fd5cde1fbc6
Raaj Kanchan <iamraajkanchan@gmail.com>
2021-12-31 17:41:14 +0530

    MainActivity updated for beginTransaction.add()

--- app/src/main/java/com/example/fragment_android/MainActivity.kt
+++ app/src/main/java/com/example/fragment_android/MainActivity.kt
@@ -9,18 +9,20 @@ import com.example.fragment_android.fragments.FragmentB
 class MainActivity : AppCompatActivity() , Communicator
 {
     private lateinit var binding : ActivityMainBinding
+    private var fragmentA : FragmentA? = null
+    private var fragmentB : FragmentB? = null
+
     override fun onCreate(savedInstanceState : Bundle?)
     {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        /*This check is used to avoid calling Fragment lifecycles twice
+        setContentView(binding.root)/*This check is used to avoid calling Fragment lifecycles twice
         * Check Reference 1 for more details
         *  */
         if (savedInstanceState == null)
         {
-            val fragmentA = FragmentA()
-            supportFragmentManager.beginTransaction().replace(R.id.fragmentMainContainer , fragmentA)
+            fragmentA = FragmentA()
+            supportFragmentManager.beginTransaction().add(R.id.fragmentMainContainer , fragmentA !!)
                 .commit()
         }
     }
@@ -30,9 +32,10 @@ class MainActivity : AppCompatActivity() , Communicator
         val bundle = Bundle().apply {
             putString("message" , editTextInput)
         }
-        val fragmentB = FragmentB()
-        fragmentB.arguments = bundle
-        supportFragmentManager.beginTransaction().replace(R.id.fragmentMainContainer , fragmentB)
-            .commit()
+        fragmentB = FragmentB()
+        fragmentB?.arguments = bundle
+        val transaction = this.supportFragmentManager.beginTransaction()
+        transaction.hide(fragmentA !!)
+        transaction.add(R.id.fragmentMainContainer , fragmentB !!).commit()
     }
 }
